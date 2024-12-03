b2b764b8e158e1020d2f71c947e490b77c3bad61
Ali Arslan <aliarslan2681@gmail.com>
2024-12-03 18:41:17 +0300

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -3,13 +3,13 @@ import firebase  from "firebase/compat/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsdfw93sbHpYNseLXE5910q3ebjNIS79s",
-  authDomain: "jobapp-1f201.firebaseapp.com",
-  projectId: "jobapp-1f201",
-  storageBucket: "jobapp-1f201.firebasestorage.app",
-  messagingSenderId: "301346611180",
-  appId: "1:301346611180:web:63f3d3e83e00a6a4486ac9",
-  measurementId: "G-GTBEPC29D6"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
